b2b26fae925666f1ed611cdd67df1282b966d313
31231024247@ueh.edu.vn <31231024247@ueh.edu.vn>
2024-11-06 16:19:24 +0700

    array 1

--- Exercise_DaoNgocHuynhAnh/Session_06.cs
+++ Exercise_DaoNgocHuynhAnh/Session_06.cs
@@ -10,22 +10,45 @@ namespace Exercise_DaoNgocHuynhAnh
     {
         public static void Main()
         {
-        Question_01();
-        Console.ReadKey();
-        }
-
-        public static void Question_01()
-        {
             Console.Write("Ban hay nhap vao so phan tu: ");
             int n = int.Parse(Console.ReadLine());
             int[] items;
             items = new int[n];
+            NhapPhanTu(items);
+            InMang(items);
+            TangMang(items);
+        }
 
+            //Nguoi dung nhap items vao array
+            static void NhapPhanTu(int[] items)
+            { 
             for (int i = 0; i < items.Length; i++)
-            {
-                Console.WriteLine("Nhap vao mot so: ");
+                {
+                    Console.Write($"Nhap vao phan tu {i + 1}: ");
+                    items[i] = int.Parse(Console.ReadLine());
+                }
             }
-            Console.WriteLine($"Vay mang co {n} phan tu gom: ");
+            //In array ra man hinh
+            static void InMang(int[] items)
+            { 
+            Console.WriteLine($"Vay mang co phan tu gom: ");
+            for(int i = 0; i < items.Length;i++)
+                {
+                    Console.WriteLine($"Phan tu {i +1}: {items[i]} ");
+                }
+            }
+
+            //Increase each item by adding it with 2
+            static void TangMang(int[] items)
+            { 
+            Console.WriteLine($"Mang sau khi cong 2: ");
+            for (int i=0; i < items.Length;i++)
+                {
+                    items[i] += 2;
+                    Console.Write($"{items[i]} ");
+                }
+            }
+
         }
     }
-}
+
