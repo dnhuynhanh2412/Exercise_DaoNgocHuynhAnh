47590ad1b6b6d48d41b5860e28c71cf01ffcb4a8
31231024247@ueh.edu.vn <31231024247@ueh.edu.vn>
2024-11-17 22:42:52 +0700

    ex multidimensional arrays

--- Exercise_DaoNgocHuynhAnh/Session_07.cs
+++ Exercise_DaoNgocHuynhAnh/Session_07.cs
@@ -12,16 +12,25 @@ namespace Exercise_DaoNgocHuynhAnh
         {
             //Matrix NxM (N,M was prompted from user) randomly
             int[,] a;
-            Console.Write("Nhap so dong: "); int rows = int.Parse(Console.ReadLine());
-            Console.Write("Nhap so cot: "); int cols = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so dong cho ma tran thuong: "); int rows = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so cot cho ma tran thuong: "); int cols = int.Parse(Console.ReadLine());
             a = new int[rows, cols];
             NhapMangNgauNhien(a, rows, cols);
             InMang(a, rows, cols);
             //Console.Write("Nhap so can tim: ");
             //int val = int.Parse(Console.ReadLine());
             //InGiaTri(a, val);
-            InGiaTriLonNhat(a);
-            TimGiaTriNhoNhat(a);
+            //InGiaTriLonNhat(a);
+            //TimGiaTriNhoNhatDong(a);
+            //TimGiaTriNhoNhatCot(a);
+            int[,] transpose;
+            transpose = new int[cols, rows];
+            //ChuyenViMang(a,transpose);
+            int[,] matrixsquare;
+            Console.Write("Nhap so dong va cot cho ma tran vuong: "); int Rows = int.Parse(Console.ReadLine());
+            int Cols = Rows;
+            matrixsquare = new int[Rows, Cols];
+            InDuongCheo(matrixsquare, Rows, Cols);
         }
 
         static void NhapMangNgauNhien(int[,] a, int rows, int cols)
@@ -59,7 +68,6 @@ namespace Exercise_DaoNgocHuynhAnh
                     {
                         Console.WriteLine($"{value} xuat hien tai dong {i} cot {j} \n");
                     }
-
                 }
             }
         }
@@ -78,21 +86,88 @@ namespace Exercise_DaoNgocHuynhAnh
                 }
             }
             Console.WriteLine($"Gia tri lon nhat la: {max}");
+            Console.WriteLine();
         }
 
-        static void TimGiaTriNhoNhat(int[,] a)
+        static void TimGiaTriNhoNhatDong(int[,] a) //Find the min value of ith row/col of the matrix.
         {
-            int min = a[0, 0];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                int minrow = a[i, 0];
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    if (a[i, j] < minrow)
+                    {
+                        minrow = a[i, j];
+                    }
+                }
+                Console.WriteLine($"Gia tri nho nhat cua dong {i} la: {minrow}");
+            }
+            Console.WriteLine();
+        }
+
+        static void TimGiaTriNhoNhatCot(int[,] a) //Find the min value of ith row/col of the matrix.
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                int mincol = a[0, j];
+                for (int i = 0; i < a.GetLength(0); i++)
+                {
+                    if (a[i, j] < mincol)
+                    {
+                        mincol = a[i, j];
+                    }
+                }
+                Console.WriteLine($"Gia tri nho nhat cua cot {j} la: {mincol}");
+            }
+            Console.WriteLine();
+        }
+
+        static void ChuyenViMang(int[,] a, int[,] transpose) //Chuyển đổi dòng thành cột (Chuyển vị)
+        {
             for (int i = 0; i < a.GetLength(0); i++)
             {
                 for (int j = 0; j < a.GetLength(1); j++)
                 {
-                    if (a[i, j] < min)
-                    {
-                        min = a[i, j];
-                    }
+                    transpose[j, i] = a[i, j];
                 }
-                Console.WriteLine($"Gia tri nho nhat cua dong {i} la: {min}");
+            }
+            
+            Console.WriteLine("Ma tran sau khi chuyen vi la: ");
+            for (int i = 0; i < transpose.GetLength(0); i++)
+            {
+                for (int j = 0; j < transpose.GetLength(1); j++)
+                {
+                    Console.Write(transpose[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void InDuongCheo(int[,] matrixsquare, int Rows, int Cols)  //Print the main/secondary diagonal values of the matrix.
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < matrixsquare.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixsquare.GetLength(1); j++)
+                {
+                    matrixsquare[i, j] = rnd.Next(0, 100) + 1; //So ngau nhien tu 1 den 100
+                    Console.Write(matrixsquare[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Duong cheo chinh cua ma tran vuong la: ");
+            for(int i = 0; i < matrixsquare.GetLength(0); i++)
+            {
+                Console.Write(matrixsquare[i,i] + "\t");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Duong cheo phu cua ma tran vuong la: ");
+            for(int i = 0; i < matrixsquare.GetLength(0); i++)
+            {
+                Console.Write(matrixsquare[matrixsquare.GetLength(0) - i - 1, i] + "\t");
             }
         }
     }
